c0fa7c48df09475a3838566fb23face11f00986d
Joeyatoe <yuseiyes4@gmail.com>
2020-05-15 09:26:14 +0900

    Revised as not using package for getting time

--- discordbot_1/App.js
+++ discordbot_1/App.js
@@ -1,39 +1,47 @@
 const Discord = require('discord.js');
-const chalk = require('chalk');
-const config = require('./config.json');
+	const client = new Discord.Client();
+const config = require("./config.json");
 require('date-utils');
-const client = new Discord.Client();
-const dt = new Date();
+
+	let hour;
+	let date;
+	let minutes;
+	let seconds;
+
 client.once('ready', () => {
-	console.log(chalk.bgCyan("Bot now ready"));
+	console.log('Ready!');
 });
 
-client.on('message', (message) => {
-	const formatted = dt.toFormat("現在時刻：西暦YYYY年MM月DD日HH24時MI分SS秒");
-	if (message.content === "!something") {
-		console.log("something");
-	}	else if (message.content === `${config.prefix}server`) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	} else if (message.content === `${config.prefix}user-info`) {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-	} else if (message.content === `${config.prefix}time`) {
-		message.channel.send(formatted);
-	} else if (message.content === `${config.prefix}test`) {
-		console.log(chalk.yellow(message.content));
-	} else if (message.content === `${config.prefix}plese_reply`) {
-		return message.reply("Hey!")
-	} else if (message.content === "fuck you") {
-		const message = message.content;
-		message.channel.send(message.toUppercase());
+client.on("message", (message) => {
+	if (message.content === "!ctg time")　{
+
+		date = new Date();
+		hour = date.getHours();
+		minutes = date.getMinutes();
+		seconds = date.getSeconds();
+
+		message.channel.send("30秒後に時刻を表示します。");
+
+		const timeoutMeth = setTimeout( () => {
+			message.channel.send(`${hour}時${minutes}分${seconds}秒`);
+		}, 30000);
+
+		client.on("message", (messagen) => {
+			if (messagen.content === `${config.prefix}cancel time`) {
+				const promise = new Promise(() => {
+					clearTimeout(timeoutMeth);
+					resolve();
+				});
+				promise.then(() => {
+					messagen.channel.send("時刻の表示をキャンセルしました。");
+				});
+			}
+		});
+	} else {
+		return;
 	}
 });
-client.on('channelCreate', message => {
-	console.log("チャンネルが作成されました。");
-});
-client.on('messageDelete', (message) => {
-	message.channel.send(`${message.createdAt}に作成された\n${message.author}のメッセージは誰かが削除しました。`);
-});
-client.on('message', (message) => {
-	if (message.content)
-})
+
+
+
 client.login(config.token);
